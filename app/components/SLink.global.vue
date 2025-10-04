bb0c8225f703aa56355e04c8b070efd719a5a3be
schplitt <schplitt.contact@gmail.com>
2025-10-04 22:20:18 +0200

    fix: update target attribute logic for NuxtLink to open external links in a new tab

--- app/components/SLink.global.vue
+++ app/components/SLink.global.vue
@@ -10,7 +10,7 @@ const { icon = '', samePage = false } = defineProps<{
 <template>
   <NuxtLink
     :to="to"
-    :target="samePage ? undefined : '_blank'"
+    :target="to?.startsWith('http') ? '_blank' : undefined"
     class="twp inline-flex items-center align-bottom gap-1 border-b border-dotted border-gray-300 hover:border-gray-600 transition-colors"
   >
     <Icon
