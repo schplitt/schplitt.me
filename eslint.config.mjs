c90a14ace9cefbaf8629c92fe86bbb8335e64542
schplitt <schplitt.contact@gmail.com>
2025-11-17 23:31:02 +0100

    feat: update ESLint configuration to enforce object property newline rule in Vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'vue/object-property-newline': ['error', { allowAllPropertiesOnSameLine: false }],
+  },
+})
